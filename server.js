ec3d409e34901caa224532aec24546dbcdfdf2f6
Alex Bozeat <alexboz101@gmail.com>
2020-02-23 12:47:06 -0500

    Setup basic express server

---
+++ server.js
@@ -0,0 +1,10 @@
+// Express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from public directory
+app.use(express.static('public'));
+
+// Start listening
+app.listen(port, () => console.log('Server started on port ' + port));

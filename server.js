a782412e1934d60ecac02ba8eb57e23116ce1fc6
Alex Bozeat <alexboz101@gmail.com>
2020-04-07 13:39:50 -0400

    Implemented .env

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// Get values from .env file
+require('dotenv').config();
+
 // Express
 const express = require('express');
 const app = express();
-const port = 3000;
 
 // Serve static files from public directory
 app.use(express.static('public'));
 
 // Start listening
-app.listen(port, () => console.log('Server started on port ' + port));
+app.listen(process.env['PORT'], () => console.log('Server started on port ' + process.env['PORT']));

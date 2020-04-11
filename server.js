0e68f06e4637aed5ad734206ffc8acf072fd5044
Unusuala1l2e3x4 <hea2@rpi.edu>
2020-04-11 12:56:20 -0400

    merge

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+// Get values from .env file
+require('dotenv').config();
+
 // Express
 const express = require('express');
 const app = express();
-const port = 3000;
 
 // Serve static files from public directory
 app.use(express.static('public'));
 
+// MongoDB
+const mongodb = require('mongodb');
+let db;
+mongodb.MongoClient.connect(process.env['MONGO_URL']).then((mongo, err) => {
+	if(err) throw err;
+	else{
+		db = mongo.db(process.env['MONGO_DB']);
+		console.log("Database connected");
+
+		// Start listening once the database has been connected
+		app.listen(process.env['PORT'], () => console.log('Server started on port ' + process.env['PORT']));
+	}
+});

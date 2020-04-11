4c98e2446675bb39be2fb9bf3039e56866ca8aa3
Alex Bozeat <alexboz101@gmail.com>
2020-04-11 15:18:25 -0400

    Begin register route

--- server.js
+++ server.js
@@ -8,6 +8,47 @@ const app = express();
 // Serve static files from public directory
 app.use(express.static('public'));
 
+// Parse responses as JSON
+app.use(express.json());
+
+// API Routes
+app.post('/api/user/register', (req, res) => {
+
+	let email = req.body['email'];
+	let password = req.body['password'];
+
+	// Check if email is present
+	if(email === undefined){
+		res.status(400).send({
+			error: "Missing email"
+		});
+	}
+	// Check if password is present
+	else if(password === undefined){
+		res.status(400).send({
+			error: "Missing password"
+		});
+	}
+	else{
+		// Check if password meets requirements (At least 12 characters)
+		if(password.length < 12){
+			res.status(400).send({
+				error: "Password is too short"
+			});
+		}
+		// Check if email is available
+		else if(db.collection("users").countDocuments({email:email}) > 0){
+			res.status(400).send({
+				error: "Email is not available"
+			});
+		}
+		// Create account
+		else{
+			res.sendStatus(200);
+		}
+	}
+});
+
 // MongoDB
 const mongodb = require('mongodb');
 let db;

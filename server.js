05b12e4c1f453821d06df578bfbc961025ff2938
Alex Bozeat <alexboz101@gmail.com>
2020-04-11 17:19:50 -0400

    Register working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Get values from .env file
 require('dotenv').config();
 
+const bcrypt = require('bcrypt');
+
 // Express
 const express = require('express');
 const app = express();
@@ -17,7 +19,7 @@ app.post('/api/user/register', (req, res) => {
 	let email = req.body['email'];
 	let password = req.body['password'];
 
-	// Check if email is present
+	// Check if email is present TODO: check if email is valid (and don't accept blank emails)
 	if(email === undefined){
 		res.status(400).send({
 			error: "Missing email"
@@ -35,16 +37,41 @@ app.post('/api/user/register', (req, res) => {
 			res.status(400).send({
 				error: "Password is too short"
 			});
-		}
-		// Check if email is available
-		else if(db.collection("users").countDocuments({email:email}) > 0){
-			res.status(400).send({
-				error: "Email is not available"
+		}else{
+			// Check if email is available
+			db.collection('users').countDocuments({email:email}).then((result) => {
+				if(result > 0){
+					res.status(400).send({
+						error: "Email is not available"
+					});
+				}else{
+					// Create the account if it is
+					bcrypt.hash(password, 10, (err, hash) => {
+						if(err){
+							res.status(500).send({
+								error: "Failed to hash password"
+							});
+							throw err;
+						}else{
+							db.collection('users').insertOne({
+								email: email,
+								password: hash
+							}).then(() => {
+								res.sendStatus(200);
+							}).catch((err) => {
+								res.status(500).send({
+									error: "Failed to insert user into database"
+								});
+								throw err;
+							});
+						}
+					});
+				}
+			}).catch((err) => {
+				res.status(500).send({
+					error: "Failed to check if email is available"
+				});
 			});
-		}
-		// Create account
-		else{
-			res.sendStatus(200);
 		}
 	}
 });
